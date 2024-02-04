cc20c38926f03200a552623222374e278adedc86
Alister <alister.messi@gmail.com>
2024-02-04 21:14:29 +0000

    Added the hangman

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,57 @@
+import styles from "../css/Keyoard.module.css";
+
+type KeyboardProps = {
+    disabled?: boolean,
+    activeLetters: string[],
+    inactiveLetters: string[],
+    addGuessedLetter: (letter: string) => void,
+}
+
+export function Keyboard({ disabled = false, activeLetters, inactiveLetters, addGuessedLetter }: KeyboardProps) {
+    return <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(50px, 1fr))',
+        gap: '.5rem',
+    }}>
+        {KEYS.map(key => {
+            const isActive = activeLetters.includes(key);
+            const isInactive = inactiveLetters.includes(key);
+
+            return <button
+                onClick={() => addGuessedLetter(key)}
+                className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`}
+                disabled={isInactive || isActive || disabled}
+                key={key}
+            >{key}</button>
+        })}
+    </div>
+}
+
+const KEYS = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
